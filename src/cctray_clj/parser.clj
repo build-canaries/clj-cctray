0a4eeb353c3042e8d3350b10f652979ed006c792
Ste <cowley05@hotmail.co.uk>
2014-08-23 16:21:35 +0100

    Ste - Spliting pipeline stage and name

--- src/cctray_clj/parser.clj
+++ src/cctray_clj/parser.clj
@@ -1,9 +1,17 @@
 (ns cctray-clj.parser
-  (:require [clojure.xml :as xml]))
+  (:require [clojure.xml :as xml]
+            [clojure.string :refer [split]]))
 
 (defn to-map [url]
   (xml/parse url))
 
 (defn get-projects [url]
   (let [content (:content (to-map url))]
-    (remove nil? (map #(if (= (:tag %) :Project) (:attrs %)) content))))
+    (remove nil? (map
+                   #(if (= (:tag %) :Project)
+                     (:attrs %))
+                   content))))
+
+(defn extract-name [name]
+  (let [split-name (split name #"\s::\s")]
+    {:name (first split-name) :pipeline (second split-name) :stage (last split-name)}))
--- test/cctray_clj/parser_test.clj
+++ test/cctray_clj/parser_test.clj
@@ -11,3 +11,7 @@
       (subject/get-projects "resources/test_data.xml") => [{:name "name1" :lastBuildStatus "Success"}
                                                            {:name "name2" :lastBuildStatus "Success"}
                                                            {:name "name3" :lastBuildStatus "Failure"}])
+
+(facts "will split project attributes to seperate attributes in map"
+       (fact "project name"
+             (subject/extract-name "name1 :: test :: deploy") => {:name "name1" :pipeline "test" :stage "deploy"}))
