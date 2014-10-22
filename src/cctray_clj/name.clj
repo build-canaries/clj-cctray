f330c4b6a8413f176bed1bace5e351f3215929fe
Christopher Martin <cmartin@thoughtworks.com>
2014-10-22 21:43:38 +0100

    chris - sentanceize the stage and job names

--- src/cctray_clj/name.clj
+++ src/cctray_clj/name.clj
@@ -2,14 +2,15 @@
   (:require [clojure.string :refer [split join]]))
 
 (defn sentanceize [input-string]
-  (-> input-string
-      (clojure.string/replace #"[-_]+", " ")
-      (clojure.string/replace #"([a-z])([A-Z])", "$1 $2")))
+  (if-not (nil? input-string)
+    (-> input-string
+        (clojure.string/replace #"[-_]+", " ")
+        (clojure.string/replace #"([a-z])([A-Z])", "$1 $2"))))
 
 (defn extract-name [name]
   (let [split-name (split name #"\s::\s")]
     {:name     (sentanceize (first split-name))
      :raw-name (first split-name)
-     :stage    (second split-name)
+     :stage    (sentanceize (second split-name))
      :job      (if (> (count split-name) 1)
-                 (last split-name))}))
+                 (sentanceize (last split-name)))}))
--- test/cctray_clj/name_test.clj
+++ test/cctray_clj/name_test.clj
@@ -15,16 +15,20 @@
 
        (fact "acroynms are not split"
              (subject/sentanceize "ABC") => "ABC"
-             (subject/sentanceize "somethingEndingWithABC") => "something Ending With ABC"))
+             (subject/sentanceize "somethingEndingWithABC") => "something Ending With ABC")
+
+       (fact "nil doesn't cause a NullPointerException"
+             (subject/sentanceize nil) => nil))
 
 (facts "extract-name"
        (fact "includes the raw name"
              (subject/extract-name "some-name") => (contains {:raw-name "some-name"}))
 
        (fact "sentanceizes the name"
-             (subject/extract-name "some-name") => (contains {:name "some name"})
+             (subject/extract-name "some-name") => (contains {:name ..name..})
              (provided
-               (subject/sentanceize "some-name") => "some name"))
+               (subject/sentanceize "some-name") => ..name..
+               (subject/sentanceize anything) => irrelevant))
 
        (facts "ThoughtWorks Go uses :: to delimit the name from the stage and job"
               (fact "extracts the name"
@@ -33,9 +37,21 @@
               (fact "extracts the stage"
                     (subject/extract-name "name :: stage") => (contains {:stage "stage"}))
 
+              (fact "sentanceizes the stage"
+                    (subject/extract-name "name :: some-stage") => (contains {:stage ..stage..})
+                    (provided
+                      (subject/sentanceize "some-stage") => ..stage..
+                      (subject/sentanceize anything) => irrelevant))
+
               (fact "extracts the job"
                     (subject/extract-name "name :: stage :: job") => (contains {:job "job"}))
 
-              (fact "doesn't error when used for non Go CI servers"
+              (fact "sentanceizes the job"
+                    (subject/extract-name "name :: stage :: some-job") => (contains {:job ..job..})
+                    (provided
+                      (subject/sentanceize "some-job") => ..job..
+                      (subject/sentanceize anything) => irrelevant))
+
+              (fact "sets stage and job to nil if no delimiter exists (ie. if parsing from non Go CI Servers)"
                     (subject/extract-name "i-am-just-a-name") => (contains {:stage nil
                                                                             :job   nil}))))
