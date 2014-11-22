4164e55b0b687df2c945f1326b8b3234464d0b78
Christopher Martin <GentlemanHal@users.noreply.github.com>
2014-11-22 23:00:44 +0000

    Update the name to be more standard with other clojure libs

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cctray-clj "0.1.0"
-            :description "XML parser for cctray"
-            :url "https://github.com/cowley05/cctray-clj"
+(defproject clj-cctray "0.1.0"
+            :description "Clojure parser for cctray.xml"
+            :url "https://github.com/build-canaries/clj-cctray"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/data.json "0.2.5"]
