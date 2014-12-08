a3bbecee8e339eae67f04e40f61a056aa5fd7148
Joe Wright <joe@joejag.com>
2014-12-08 00:09:00 +0000

    Joe closes #4 add timeout. Also force application/xml needed by Travis

--- src/clj_cctray/reader.clj
+++ src/clj_cctray/reader.clj
@@ -1,15 +1,19 @@
 (ns clj-cctray.reader
   (:require [org.httpkit.client :as httpkit]))
 
+(def ^:private http-kit-options {:insecure? true
+                                 :timeout 30000
+                                 :headers {"Accept" "application/xml"}})
+
 (defn http-read-unsecured-httpkit [url]
-  (:body @(httpkit/get url {:insecure? true})))
+  (:body @(httpkit/get url http-kit-options)))
 
 (defn http-read-unsecured->String [url]
   (let [raw-response (http-read-unsecured-httpkit url)]
     (java.io.ByteArrayInputStream. (.getBytes raw-response))))
 
 (defn decide-reader [url]
-  (if (.startsWith url "https") :httpkit :standard))
+  (if (.startsWith (.toLowerCase url) "http") :httpkit :standard))
 
 (def readers {:httpkit http-read-unsecured->String
               :standard identity})
