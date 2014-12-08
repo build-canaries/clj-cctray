ef4e1d14725021b3501b88eeb70eaa7c508a4ec8
Joe Wright <joe@joejag.com>
2014-12-08 22:20:03 +0000

    Joe - version number bump. breaking changes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-cctray "0.5.1"
+(defproject clj-cctray "0.6.0"
             :description "Clojure parser for cctray.xml"
             :url "https://github.com/build-canaries/clj-cctray"
             :license {:name "Eclipse Public License"
