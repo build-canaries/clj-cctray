247beea5bb621407ae1a26edb716cba56b7718b1
Christopher Martin <cmartin@thoughtworks.com>
2015-07-28 23:25:05 +0100

    Chris - updates version and deps

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-cctray "0.8.0"
+(defproject clj-cctray "0.9.0"
             :description "Clojure parser for cctray.xml"
             :url "https://github.com/build-canaries/clj-cctray"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[clj-time "0.9.0"]]
+            :dependencies [[clj-time "0.10.0"]]
             :profiles {:dev {:plugins      [[lein-midje "3.1.3"]
-                                            [lein-ancient "0.5.5"]
-                                            [codox "0.8.10"]]
-                             :dependencies [[org.clojure/clojure "1.6.0"]
-                                            [midje "1.6.3"]]}})
+                                            [lein-ancient "0.6.7"]
+                                            [codox "0.8.13"]]
+                             :dependencies [[org.clojure/clojure "1.7.0"]
+                                            [midje "1.7.0"]]}})
