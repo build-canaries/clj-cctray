3de5a7c81adb304f3f245af8b9570ce51b3f83f6
Christopher Martin <eternalepoch@gmail.com>
2017-02-25 14:51:14 +0000

    updates deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/build-canaries/clj-cctray"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-time "0.11.0"]]
+  :dependencies [[clj-time "0.13.0"]]
   :aliases {"test" ["midje"]}
   :codox {:output-path "doc"}
-  :profiles {:dev {:plugins      [[lein-midje "3.2"]]
+  :profiles {:dev {:plugins      [[lein-midje "3.2.1"]]
                    :dependencies [[org.clojure/clojure "1.8.0"]
                                   [midje "1.8.3"]]}})
