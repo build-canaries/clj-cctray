afbae45c7d88680266d96f44dae7ef24806f123d
Christopher Martin <eternalepoch@gmail.com>
2017-11-05 22:29:56 +0000

    adds api docs to master instead of using gh-pages branch

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-time "0.14.0"]]
   :aliases {"test" ["midje"]}
-  :codox {:output-path "doc"}
+  :codox {:output-path "docs"}
   :profiles {:dev {:plugins      [[lein-midje "3.2.1"]]
                    :dependencies [[org.clojure/clojure "1.8.0"]
                                   [midje "1.8.3"]]}})
