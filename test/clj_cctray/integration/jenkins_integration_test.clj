0fd8fb6b9e33f3bb7cd44a974be47f8d0d77ed70
Christopher Martin <cmartin@thoughtworks.com>
2018-08-05 14:54:44 +0100

    fixes broken test

--- test/clj_cctray/integration/jenkins_integration_test.clj
+++ test/clj_cctray/integration/jenkins_integration_test.clj
@@ -9,16 +9,11 @@
       (.exists (io/as-file test-data-url)) => true)
 
 (fact "will create list of projects"
-      (subject/get-projects test-data-url {:server :go :normalise true}) => (has every? (contains {:name               string?
-                                                                                                   :unnormalised-name  string?
-                                                                                                   :activity           keyword?
-                                                                                                   :prognosis          keyword?
-                                                                                                   :last-build-status  keyword?
-                                                                                                   :last-build-label   string?
-                                                                                                   :last-build-time    anything
-                                                                                                   :next-build-time    anything
-                                                                                                   :web-url            string?
-                                                                                                   :stage              string?
-                                                                                                   :unnormalised-stage string?
-                                                                                                   :job                string?
-                                                                                                   :unnormalised-job   string?})))
+      (subject/get-projects test-data-url {:server :jenkins}) => (has every? (contains {:name              string?
+                                                                                        :activity          keyword?
+                                                                                        :prognosis         keyword?
+                                                                                        :last-build-status keyword?
+                                                                                        :last-build-label  string?
+                                                                                        :last-build-time   anything
+                                                                                        :next-build-time   anything
+                                                                                        :web-url           string?})))
