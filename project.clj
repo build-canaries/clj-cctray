99faf99fb627e6d16ece3209c6fa554cbe6815b6
Christopher Martin <cmartin@thoughtworks.com>
2018-08-20 13:48:55 +0100

    [#22] stop publishing from CI as a temp fix
    I'll manually deploy the new version to Clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-cctray "1.0.1"
+(defproject clj-cctray "1.0.2"
   :description "Clojure parser for cctray.xml"
   :url "https://github.com/build-canaries/clj-cctray"
   :license {:name "Eclipse Public License"
