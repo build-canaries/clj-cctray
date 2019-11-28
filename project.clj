d05fa0545738d94582957521db6dde880ce74342
Christopher Martin <eternalepoch@gmail.com>
2019-11-28 20:38:43 +0000

    updates deps

--- project.clj
+++ project.clj
@@ -5,19 +5,20 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clojure.java-time "0.3.2"]]
   :javac-options ["-Dclojure.compiler.direct-linking=true"]
-  :aliases {"lint"     ["with-profile" "+test" "eastwood"]
-            "coverage" ["with-profile" "+test" "cloverage"]
-            "pre-push" ["do" "clean," "lint," "test," "coverage"]}
+  :aliases {"lint"          ["with-profile" "+test" "eastwood"]
+            "coverage"      ["with-profile" "+test" "cloverage"]
+            "check-updates" ["ancient" ":all"]
+            "pre-push"      ["do" "clean," "lint," "test," "coverage"]}
   :repositories [["releases" {:url           "https://clojars.org/repo"
                               :username      :env/clojars_username
                               :password      :env/clojars_password
                               :sign-releases false}]]
   :codox {:output-path "docs"}
   :profiles {:dev {:plugins      [[lein-ancient "0.6.15"]
-                                  [jonase/eastwood "0.3.5"]
-                                  [lein-cloverage "1.1.0"]
-                                  [lein-codox "0.10.6"]]
-                   :dependencies [[org.clojure/clojure "1.10.0"]]}}
+                                  [jonase/eastwood "0.3.6"]
+                                  [lein-cloverage "1.1.2"]
+                                  [lein-codox "0.10.7"]]
+                   :dependencies [[org.clojure/clojure "1.10.1"]]}}
   :cloverage {:output "target/coverage-reports"
               :junit? true}
   :pom-location "target/")
